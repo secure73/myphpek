90683a051552b078f197a23e077e88775adb7267
secure73 <secure73@gmail.com>
2023-03-30 15:10:32 +0100

    code correction for login.php

--- login.php
+++ login.php
@@ -1,22 +1,36 @@
-<?php 
-include('./template/navbar.php'); 
+<?php
+include('./template/navbar.php');
+
+if(isset($_POST['email']))
+{
+  $email = $_POST['email'];
+  $password  = $_POST['password'];
+}
+
+
+//1: find user by email
+// 2: check email password if correct is
+//3: if correct redirect to profile.php
 
 ?>
 <div class="m-3">
-<h1>Login</h1>
-<div class="card flex m-5 p-5 bg-secondary bg-opacity-10">
-<form action="" class="">
-<div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  <h1>Login</h1>
+
+  <div class="card flex m-5 p-5 bg-secondary bg-opacity-10">
+    <form method="post" class="">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+          placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input name="password" type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+      <br>
+      <button type="submit" class="btn btn-primary">Submit</button>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <br>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</div>
+</form>
 </div>
 <?php include('./template/footer.php'); ?>
